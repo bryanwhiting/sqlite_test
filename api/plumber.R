2f6b6b9c37e6a310a1fabd01bf89fd3ac896cb11
Bryan Whiting <bryan.g.whiting@gmail.com>
2021-10-10 22:28:53 +0000

    Finished plumber API

--- api/plumber.R
+++ api/plumber.R
@@ -1,4 +1,40 @@
 # plumber.R
+library(dplyr)
+library(tidyr)
+library(stringr)
+df <- readr::read_csv('data.csv') %>%
+  mutate(
+    x = str_extract(s, "\\w[0-9]*"),
+    s_clean = ifelse(is.na(x), s, x)
+  ) %>%
+  select(id, s_clean, s, everything()) %>%
+  select(-x)
+
+#* Prints dataframe of serial numbers
+#* @param serials 
+#* @post /inspect
+process_data <- function(serials = ""){
+  if (length(serials) > 0){
+    if(!(is.character(serials))){stop("Did not pass in string of form 's1|s2'")}
+    df <- df %>% 
+      filter(str_detect(s_clean, serials))
+    if (nrow(df) == 0){
+      return("ERROR: NO SERIALS WITH THAT SPECIFICATION.")
+    }
+  }
+  
+  out <- df %>%
+    # filter(str_detect(s, paste(keep, collapse="|"))) %>%
+    filter(!(s %in% (df %>% filter(p == "sub") %>% distinct(s) %>% pull()))) %>%
+    group_by(s_clean) %>%
+    mutate(keep_s = row_number() == 1) %>%
+    group_by(s) %>%
+    filter(max(keep_s) == TRUE) %>%
+    select(s, s_clean, p, v) %>%
+    pivot_wider(names_from=p, values_from=v)
+}
+
+
 
 #* Echo back the input
 #* @param msg The message to echo
